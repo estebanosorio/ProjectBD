81021756391e3f1c20254d6750b831e3a6492c9e
esteban <esteban.rpo@hotmail.com>
2020-10-07 20:36:28 -0500

    commit dos

--- src/main/java/com/bdatos/prubasedatoswild/Demo1Application.java
+++ src/main/java/com/bdatos/prubasedatoswild/Demo1Application.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Demo1Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Demo1Application.class, args);
 	}
 	@Bean
